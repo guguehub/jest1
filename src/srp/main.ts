cbc88f6b10a53d8727a9cd32ad4b65fc02998897
Luis Gustavo <dekmis@hotmail.com>
2023-07-25 18:11:29 -0300

    eslint e prettier update, messaging.ts created

--- src/srp/main.ts
+++ src/srp/main.ts
@@ -1,14 +1,17 @@
 import { ShoppingCart } from './shopping-cart';
+import { Order } from './order';
 
 const shoppingCart = new ShoppingCart();
+const order = new Order(shoppingCart);
 
 shoppingCart.addItem({ name: 'amaciante', price: 10 });
 shoppingCart.addItem({ name: 'lapis', price: 5 });
 shoppingCart.addItem({ name: 'caderno', price: 8 });
 
-//shoppingCart.clear();
 console.log(shoppingCart.items);
-shoppingCart.checkout();
 
 console.log('o valor total do pedido: ' + shoppingCart.total());
-console.log(shoppingCart.orderStatus);
+console.log(order.orderStatus);
+order.checkout();
+//shoppingCart.clear();
+console.log(order.orderStatus);
--- src/srp/order.ts
+++ src/srp/order.ts
@@ -1,3 +1,35 @@
-export Class Order {
-  
+import { OrderStatus } from './interfaces/order-status';
+import { ShoppingCart } from './shopping-cart';
+
+export class Order {
+  private _orderStatus: OrderStatus = 'open';
+
+  // injetar dependencia
+  constructor(private readonly cart: ShoppingCart) {}
+
+  get orderStatus(): OrderStatus {
+    return this._orderStatus;
+  }
+
+  checkout(): void {
+    if (this.cart.isEmpty()) {
+      console.log('seu carrinho esta vazio');
+      return;
+    }
+
+    this.sendMessage(
+      `seu pedido com total de ${this.cart.total()} foi recebido`,
+    );
+
+    this.saveOrder();
+    this.cart.clear();
+  }
+
+  sendMessage(msg: string): void {
+    console.log('mensage enviada', msg);
+  }
+
+  saveOrder(): void {
+    console.log(' pedido salvo com sucesso');
+  }
 }
