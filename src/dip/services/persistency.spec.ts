c3811ec8cfec6ad0ada4648fe7f1ba03b9d8a202
Luis Gustavo <dekmis@hotmail.com>
2023-08-21 17:37:27 -0300

    testes jest configuraçao ok

---
+++ src/dip/services/persistency.spec.ts
@@ -0,0 +1,9 @@
+//spec testes unitarios, it testes ingegraçao
+it('descriçao do teste (IT)', () => {
+  const number = 1;
+  expect(number).toBe(1);
+});
+test('teste feito com TEST', () => {
+  const nome = "Luis";
+  expect(nome).toBe('Luis');
+});
