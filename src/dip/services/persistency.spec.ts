20865597ac9025dd6f2a394429cf8212450fe037
Luis Gustavo <dekmis@hotmail.com>
2023-08-22 12:05:14 -0300

    jest test persistency

--- src/dip/services/persistency.spec.ts
+++ src/dip/services/persistency.spec.ts
@@ -1,15 +1,26 @@
-//spec testes unitarios, it testes ingegraçao
+import { Persistency } from "./persistensy";
 
-describe( 'Teste 1 , numero', () => {
-  it('it should return 1 ', () => {
-    const number = 1;
-    expect(number).toBe(1)
+describe('Persistency test', () => {
+afterEach(() => jest.clearAllMocks());
+
+  it('should return undefined', () => {
+    //sut = system under test
+    const sut = new Persistency();
+    expect(sut.saveOrder()).toBeUndefined();
+  });
+
+  it('should call console.log', () => {
+
+    const sut = new Persistency();
+    const consoleSpy = jest.spyOn(console,'log');
+    sut.saveOrder();
+    expect(consoleSpy).toHaveBeenCalledTimes(1);
+  });
+  it('should call console.log with  "pedido salvo com sucesso" ', () => {
+  
+    const sut = new Persistency();
+    const consoleSpy = jest.spyOn(console,'log');
+    sut.saveOrder();
+    expect(consoleSpy).toBeCalledWith(' pedido salvo com sucesso')
   });
 });
-
-describe('this test should return (name)= luiz', () => {
-  test('teste feito com TEST', () => {
-    const nome = "Luis";
-    expect(nome).toBe('Luis');
-  });
-})
